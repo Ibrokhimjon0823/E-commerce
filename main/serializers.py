eefb801a8a2be7f08fb294df923f0d284862ef50
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2022-09-25 16:43:41 +0500

    added serializers.py

---
+++ main/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Category
+        fields = ("id", "name", "description")
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Product
+        fields = "__all__"
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Payment
+        fields = ("id", "payment_type")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Order
+        fields = "__all__"
